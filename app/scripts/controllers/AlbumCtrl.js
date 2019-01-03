778c96c0401428b16dc190559ab167fee031ad95
nahome <nahom.teshome@yahoo.com>
2019-01-02 23:27:07 -0800

    clear the starter project

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,1 @@
-(function() {
-     function AlbumCtrl(Fixtures, SongPlayer) {
-       this.albumData = Fixtures.getAlbum();
-       this.songPlayer = SongPlayer;
-     }
 
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
- })();
--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,1 @@
-(function() {
-     function CollectionCtrl(Fixtures) {
-       this.albums = Fixtures.getCollection(12);
-     }
 
-     angular
-         .module('blocJams')
-         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
- })();
