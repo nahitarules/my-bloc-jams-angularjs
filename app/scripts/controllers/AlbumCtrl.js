366de126c085d6d2ad9107d2f2f441922b41992c
nahome <nahom.teshome@yahoo.com>
2019-01-04 20:50:00 -0800

    controllers assignment complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,1 +1,10 @@
+(function() {
+     function AlbumCtrl() {
 
+       this.albumData = angular.copy(albumPicasso);
+     }
+
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
