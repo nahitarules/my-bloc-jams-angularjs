84576a782341ee190d0fc2f1ba22e303c903e453
nahome <nahom.teshome@yahoo.com>
2019-01-05 19:59:30 -0800

    services part2 assignment complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -25,18 +25,20 @@
             currentSong = song;
           };
 
+          var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+          }
 
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
              setSong(song);
 
-             currentBuzzObject.play();
-             song.playing = true;
+             playSong(song);
 
            } else if (currentSong === song) {
                if (currentBuzzObject.isPaused()) {
-                   currentBuzzObject.play();
-                   song.playing = true;
+                   playSong(song);
                  }
                }
            };
